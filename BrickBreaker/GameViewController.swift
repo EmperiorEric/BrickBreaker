93c513359ce46ebcd0f3f2cbef7fe1849fbd98c8
Ryan Poolos <ryanpoolos@Gmail.com>
2018-07-31 19:53:28 -0400

    Balls and Bricks respect the floor.

--- BrickBreaker/GameViewController.swift
+++ BrickBreaker/GameViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+enum CollisionCategory: Int {
+    case ball = 2
+    case block = 4
+    case floor = 8
+}
+
+extension SCNPhysicsBody {
+    var collisionCategory: [CollisionCategory] {
+        get {
+            return []
+        }
+        set {
+//            collisionBitMask = collisionCategory.map({$0.rawValue})
+        }
+    }
+}
+
+class GameViewController: UIViewController, SCNPhysicsContactDelegate {
 
     override func loadView() {
         self.view = SCNView()
@@ -19,6 +36,22 @@ class GameViewController: UIViewController {
     var scnView: SCNView {
         return self.view as! SCNView
     }
+
+    lazy var floor: SCNNode = {
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor(hue: .random(in: 0...255), saturation: 0.5, brightness: 0.5, alpha: 1)
+
+        let plane = SCNPlane(width: 20, height: 40)
+        plane.materials = [material]
+
+        let floor = SCNNode(geometry: plane)
+        floor.eulerAngles.x = .pi / -2.0
+        floor.physicsBody = SCNPhysicsBody.kinematic()
+        floor.physicsBody?.categoryBitMask = CollisionCategory.floor.rawValue
+        floor.physicsBody?.collisionBitMask = CollisionCategory.ball.rawValue | CollisionCategory.block.rawValue
+
+        return floor
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,21 +97,21 @@ class GameViewController: UIViewController {
         // configure the view
         scnView.backgroundColor = UIColor.black
 
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor(hue: .random(in: 0...255), saturation: 1, brightness: 1, alpha: 1)
-
-        let plane = SCNPlane(width: 100, height: 100)
-        plane.materials = [material]
-
-        let floor = SCNNode(geometry: plane)
-//        floor.physicsBody = SCNPhysicsBody.kinematic()
         scene.rootNode.addChildNode(floor)
+        scene.physicsWorld.contactDelegate = self
         
         // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(gestureRecognizer:)))
         scnView.addGestureRecognizer(tapGesture)
 
         layoutDemoBlocks()
+
+        let displayLink = CADisplayLink(target: self, selector: #selector(gameloop))
+        displayLink.add(to: RunLoop.main, forMode: .tracking)
+    }
+
+    @objc func gameloop() {
+        addBall(at: view.center)
     }
 
     func layoutDemoBlocks() {
@@ -91,47 +124,72 @@ class GameViewController: UIViewController {
                 box.materials = [material]
 
                 let node = SCNNode(geometry: box)
-                node.position = SCNVector3(x * 2 - 7, 1, z - 10)
+                node.position = SCNVector3(x * 2 - 7 + (z % 2), 0, z - 10)
                 node.physicsBody = SCNPhysicsBody.dynamic()
+                node.physicsBody?.mass = 100
+                node.physicsBody?.categoryBitMask = CollisionCategory.block.rawValue
+                node.physicsBody?.collisionBitMask = CollisionCategory.floor.rawValue | CollisionCategory.ball.rawValue
 
                 scnView.scene?.rootNode.addChildNode(node)
             }
         }
     }
     
-    @objc func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
+    @objc func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            let position = gestureRecognizer.location(in: scnView)
+
+            addBall(at: position)
         }
+    }
+
+    func addBall(at point: CGPoint) {
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor(hue: .random(in: 0...255), saturation: 1, brightness: 1, alpha: 1)
+
+        let geometry = SCNSphere(radius: 0.25)
+        geometry.materials = [material]
+
+        let node = SCNNode(geometry: geometry)
+        node.position = SCNVector3((point.x - view.frame.midX) / view.frame.width * 10, 0.1, 10)
+        node.physicsBody = SCNPhysicsBody.dynamic()
+        node.physicsBody?.restitution = 1.0
+        node.physicsBody?.rollingFriction = 0.1
+        node.physicsBody?.categoryBitMask = CollisionCategory.ball.rawValue
+        node.physicsBody?.collisionBitMask = CollisionCategory.floor.rawValue | CollisionCategory.block.rawValue
+
+        scnView.scene?.rootNode.addChildNode(node)
+
+        node.physicsBody?.applyForce(SCNVector3(0, 0, -30), asImpulse: true)
+    }
+
+    func addBox(atPoint point: CGPoint) {
+        let geometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
+
+        let node = SCNNode(geometry: geometry)
+        node.position = convertPoint2Dto3D(point)
+        scnView.scene?.rootNode.addChildNode(node)
+
+        node.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+    }
+
+    func convertPoint3Dto2D(_ point: SCNVector3) -> CGPoint {
+        let projectedPoint = scnView.projectPoint(point)
+
+        return CGPoint(x: Double(projectedPoint.x), y: Double(projectedPoint.y))
+    }
+
+    func convertPoint2Dto3D(_ point: CGPoint) -> SCNVector3 {
+        // The zeropoint for the z isn't actually even, so grab our origin.
+        let projectedOrigin = scnView.projectPoint(SCNVector3Zero)
+
+        // Convert to 3D with our x/y plus the origin z
+        let touchPoint = SCNVector3(x: Float(point.x), y: Float(point.y), z: projectedOrigin.z)
+
+        // unproject it to 3D space
+        let convertedPoint = scnView.unprojectPoint(touchPoint)
+
+        return convertedPoint
     }
     
     override var shouldAutorotate: Bool {
@@ -150,4 +208,29 @@ class GameViewController: UIViewController {
         }
     }
 
+    //==========================================================================
+    // MARK: - SCNPhysicsContactDelegate
+    //==========================================================================
+
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        // TODO: Show Explosion Particle or something
+    }
+
+    func physicsWorld(_ world: SCNPhysicsWorld, didEnd contact: SCNPhysicsContact) {
+        func handle(node: SCNNode) {
+            print(node)
+
+            guard let category = CollisionCategory(rawValue: node.categoryBitMask) else {
+                return
+            }
+
+            // TODO: Remove Block or Reduce Health
+            if category == .block {
+                node.removeFromParentNode()
+            }
+        }
+
+        handle(node: contact.nodeA)
+        handle(node: contact.nodeB)
+    }
 }
